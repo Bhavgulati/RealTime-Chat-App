de93060121cbe1f49611c9f51090ec380c39c5a7
Anurag <anuragsingh12698@gmail.com>
2025-05-24 00:25:28 +0530

    Completed logout path and Added Icons to Navbar with links

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,7 +2,7 @@ import {useState} from "react"
 import {useAuthStore} from "../store/useAuthStore"
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
 import AuthImagePattern from "../components/AuthImagePattern"
 
@@ -16,13 +16,13 @@ const SignUpPage = () =>{
   });
   const {signup, isSigningUp} = useAuthStore();
   
-  const validateForm = () =>{
-    if(!formData.fullname.trim()) return toast.error("Full name is required");
-    if(!formData.email.trim()) return toast.error("Email is required");
+  const validateForm = () => {
+    if (!formData.fullName.trim()) return toast.error("Full name is required");
+    if (!formData.email.trim()) return toast.error("Email is required");
     if (!/\S+@\S+\.\S+/.test(formData.email)) return toast.error("Invalid email format");
     if (!formData.password) return toast.error("Password is required");
     if (formData.password.length < 6) return toast.error("Password must be at least 6 characters");
-    
+
     return true;
   };
 
@@ -51,23 +51,23 @@ const SignUpPage = () =>{
               <p className="text-base-content/60">Get started with your free account</p>
             </div>     
           </div>
-          <form onSubmit ={handleSubmit} className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div className="form-control">
-                <label className="label">
-                  <span className="label-text font-medium">Full Name</span>
-                </label>
-            <div className="relative">
-              <div className ="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                <User className="size-5 text-base-content/40" />
-              </div>
-              <input
-                type="text"
-                className={`input input-bordered w-full pl-10 `}
-                placeholder = "John Doe"
-                value={formData.fullName}
-                onChange={(e) => setFormData({ ...formData, fullName: e.target.value})}
+              <label className="label">
+                <span className="label-text font-medium">Full Name</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <User className="size-5 text-base-content/40" />
+                </div>
+                <input
+                  type="text"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="John Doe"
+                  value={formData.fullName}
+                  onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
                 />
-            </div>
+              </div>
             </div>
 
             <div className="form-control">
