1d2cc7d73f55b638efbc760b7b98d5e73d224399
Bhavishya Gulati <bhavishyagulati@Bhavishyas-MacBook-Air.local>
2025-05-24 02:42:22 +0530

    middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -101,7 +101,7 @@ export const updateProfile = async (req, res) => {
       { new: true }
     );
 
-    res.status(200).json(updatedUser);
+    res.stat us(200).json(updatedUser);
   } catch (error) {
     console.log("error in update profile:", error);
     res.status(500).json({ message: "Internal server error" });
