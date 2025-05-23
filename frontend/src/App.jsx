1bb2a3b97cef8557ca06384fffd68642e263fb19
Anurag <anuragsingh12698@gmail.com>
2025-05-23 18:22:17 +0530

    Fixed minor errors in router and added axios support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,25 @@
 import React from 'react'
-import Navbar from "./components/Navbar";
-
-import HomePage from "./pages/HomePage";
-import SignUpPage from "./pages/SignUpPage";  
-import LoginPage from "./pages/LoginPage";
-import SettingsPage from "./pages/SettingsPage";
-import ProfilePage from "./pages/ProfilePage";
-
-
-import './app.css'
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
+import SettingsPage from './pages/SettingsPage'
+import SignUpPage from './pages/SignUpPage' 
 
 const App = () => {
   return (
     <div>
-
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/signup" element={<SignUpPage/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/settings" element={<SettingsPage/>}/>
-        <Route path="/profile" element={<ProfilePage/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login " element = {<SignUpPage />} />
+
       </Routes>
     </div>
-)}
+  )
+}
 
 export default App
