751696ea5361db290e9d89399dd7729059c2d57d
amanpand3y <aman.20234186@mnnit.ac.in>
2025-05-24 19:17:14 +0530

    Initialised Socket server in Index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,10 @@ import {connectDB} from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import {app,server} from "./lib/socket.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json());
@@ -19,7 +19,7 @@ app.use("/api/message",messageRoutes);
 
 
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
   console.log("server is running on port PORT" + PORT);
   connectDB();
 });
