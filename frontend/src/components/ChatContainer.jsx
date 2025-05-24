f3841312ca5b80effacf1e241ec76ca9df1dc1a1
Anurag <anuragsingh12698@gmail.com>
2025-05-24 16:44:53 +0530

    Message Input componenet completed

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,7 +3,7 @@ import {useEffect} from "react";
 
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
-
+import MessageSkeleton from "./skeletons/MessageSkeleton";
 const ChatContainer = ()=>{
     const {messages,getMessages,isMessagesLoading,selectedUser} = useChatStore()
     useEffect(()=>{
@@ -11,7 +11,13 @@ const ChatContainer = ()=>{
     },[selectedUser._id],getMessages)
 
 
-    if(isMessagesLoading) return <div> Loading...</div>
+    if(isMessagesLoading) return(
+        <div className = "flex-1 flex flex-col overflow-auto">
+            <ChatHeadre/>
+            <MessageSkeleton/>
+            <MessageInput/>
+        </div>
+    )
 
 
     return (
