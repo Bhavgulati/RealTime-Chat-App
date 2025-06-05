66c5f662dfe63ffccbf1496c458252219be3903c
Bhavishya Gulati <bhavishyagulati@Bhavishyas-MacBook-Air.local>
2025-06-06 00:29:23 +0530

    deploying change

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,11 @@
 import express from "express";
 import {protectRoute} from "../middleware/auth.middleware.js";
-import { getUsersForSidebar,getMessages,sendMessage} from "../controllers/message.controller.js";
+import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/users",protectRoute,getUsersForSidebar);
-router.get("/:id",protectRoute,getMessages);
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/messages/:id", protectRoute, getMessages);  // More specific path
+router.post("/send/:id", protectRoute, sendMessage);
 
-router.post("/send/:id",protectRoute,sendMessage);
 export default router;
